2b9a8b924abf823111c3f0237e2a52018984ccb8
Eric McKinney <mckinney99@gmail.com>
2018-01-04 20:19:23 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,8 @@ class Genre < ActiveRecord::Base
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
 
+  as_many :artists, through: :songs
   has_many :song_genres
   has_many :songs, through: :song_genres
-  has_many :artists, through: :songs
+
 end
